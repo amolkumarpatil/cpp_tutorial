8a82c1cbef11a85df0ac37eb2d695022684f2206
amolkumarpatil <you@example.com>
2020-10-11 11:44:39 +0530

    added changes to tut6

--- tut6.cpp
+++ tut6.cpp
@@ -2,14 +2,10 @@
 #include<iostream>
  using namespace std;
 
- int main()
- {
-     int num1, num2;
+void arithmetic(int num1, int num2)
+{
+     cout << "Following are the types of arithmetic operators in cpp: "<<endl;
      
-     cout << "Following are the types of operators in cpp: "<<endl;
-     cout<< "Enter numbers : "<<endl;
-     cin>> num1;
-     cin>> num2;
      // Addition
      cout<< "Addition of number is :"<< num1 + num2 <<endl;
      // Subtraction
@@ -25,6 +21,38 @@
      cout << "Increment of num1 is :"<< ++num1  <<endl;
      cout << "Decrement of num1 is :"<< num1 --  <<endl;
      cout << "Decrement of num1 is :"<< --num1  <<endl;
-     return 0;
+}
+
+void assignment(int num1, int num2)
+{
+    // Comparison operators
+    cout<<"Following are the comparison operators in C++"<<endl;
+    cout<<"The value of num1 == num2 is "<<(num1==num2)<<endl;
+    cout<<"The value of num1 != num2 is "<<(num1!=num2)<<endl;
+    cout<<"The value of num1 >= num2 is "<<(num1>=num2)<<endl;
+    cout<<"The value of num1 <= num2 is "<<(num1<=num2)<<endl;
+    cout<<"The value of num1 > num2 is "<<(num1>num2)<<endl;
+    cout<<"The value of num1 < num2 is "<<(num1<num2)<<endl; 
+}
+
+void logical(int num1, int num2)
+{
+    // Logical operators
+    cout<<"Following are the logical operators in C++"<<endl;
+    cout<<"The value of this logical and operator ((num1==num2) && (num1<num2)) is:"<<((num1==num2) && (num1<num2))<<endl; 
+    cout<<"The value of this logical or operator ((num1==num2) || (num1<num2)) is:"<<((num1==num2) || (num1<num2))<<endl; 
+    cout<<"The value of this logical not operator (!(num1==num2)) is:"<<(!(num1==num2))<<endl; 
+}
+
+ int main()
+ {
+    int num1, num2;
+    cout<< "Enter numbers : "<<endl;
+    cin>> num1;
+    cin>> num2;
+    arithmetic(num1, num2);
+    assignment(num1, num2);
+    logical(num1, num2);
+    return 0;
 
  }
